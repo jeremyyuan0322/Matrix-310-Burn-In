acd5577e0319b907095d0bbb848e639d8500cd3d
jeremyyuan0322 <jeremy.yuan0322@gmail.com>
2023-02-21 17:10:55 +0800

    2/21

---
+++ Matrix-310-Burn-In.ino
@@ -0,0 +1,33 @@
+/*
+Use digitalRead() to read the value from a specified digital pin, either HIGH or LOW.
+
+DIO pin define:
+DO1->pin 25
+DI1->pin 35
+DI2->pin 39
+*/
+#include "src/Artila-Matrix310.h"
+
+int di1_status = 0;
+int di2_status = 0;
+
+void readDI(){
+    di1_status = digitalRead(DI1);
+    di2_status = digitalRead(DI2);
+    
+    Serial.println((String)"DI1: " + di1_status);
+    Serial.println((String)"DI2: " + di2_status);
+}
+
+void setup() {
+    pinMode(DO1, OUTPUT);
+    pinMode(DI1, INPUT);
+    pinMode(DI2, INPUT);
+    Serial.begin(115200);
+    digitalWrite(DO1, HIGH);
+    readDI();
+}
+
+void loop() {
+
+}
